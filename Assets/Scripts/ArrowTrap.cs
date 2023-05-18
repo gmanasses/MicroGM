c3047ab620ef621a12d969579bc9f12272d649c6
Guilherme Manassés <guilhermemanasses98@gmail.com>
2023-05-18 10:54:32 -0300

    Initial commit

---
+++ Assets/Scripts/ArrowTrap.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ArrowTrap : MonoBehaviour {
+    
+    // --- Private Declarations ---
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# GameMechanics
+ A project to make many game mechanics to show my game programing abilities
