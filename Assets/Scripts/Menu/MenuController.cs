84f4024b0248b7ec8511a278fc4fc725c8fb7ff0
Guilherme Manassés <102996277+gmanasses@users.noreply.github.com>
2024-02-05 21:59:08 -0300

    Update Main Menu

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -6,20 +6,65 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour {
 
     // --- Private Declarations ---
-    [SerializeField] private GameObject _loadingScreen, _menuBarsTriggers;
+    [SerializeField] private GameObject _mainScreen;
+    [SerializeField] private GameObject _loadingScreen;
+    [SerializeField] private GameObject _settingsScreen;
+    [SerializeField] private GameObject _creditsScreen;
+    [SerializeField] private GameObject _exitPopUp, _exitButton;
 
 
     // --- Core Functions ---
     private void Start() {
-        _menuBarsTriggers.SetActive(true);
+        _mainScreen.SetActive(true);
+        _exitButton.SetActive(true);
         _loadingScreen.SetActive(false);
+        _settingsScreen.SetActive(false);
+        _creditsScreen.SetActive(false);
+        _exitPopUp.SetActive(false);
     }
 
 
     // --- Functions ---
+    public void ShowSettingsScreen() {
+        _settingsScreen.SetActive(true);
+        _mainScreen.SetActive(false);
+        _exitButton.SetActive(false);
+    }
+
+    public void HideSettingsScreen() {
+        _mainScreen.SetActive(true);
+        _settingsScreen.SetActive(false);
+        _exitButton.SetActive(true);
+    }
+
+    public void ShowCreditsScreen() {
+        _creditsScreen.SetActive(true);
+        _mainScreen.SetActive(false);
+        _exitButton.SetActive(false);
+    }
+
+    public void HideCreditsScreen() {
+        _mainScreen.SetActive(true);
+        _creditsScreen.SetActive(false);
+        _exitButton.SetActive(true);
+    }
+
+    public void ShowExitPopUp() {
+        _mainScreen.SetActive(false);
+        _exitButton.SetActive(false);
+        _exitPopUp.SetActive(true);
+    }
+
+    public void HideExitPopUp() {
+        _mainScreen.SetActive(true);
+        _exitButton.SetActive(true);
+        _exitPopUp.SetActive(false);
+    }
+
     public void ShowLoadingScreen() {
-        _menuBarsTriggers.SetActive(false);
         _loadingScreen.SetActive(true);
+        _mainScreen.SetActive(false);
+        _exitButton.SetActive(false);
     }
 
     public void LoadLevel(int SceneIndex) {
